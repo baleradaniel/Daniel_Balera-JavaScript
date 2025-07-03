98618cf28cba7541dc51e2aaa455f42222b4657a
baleradaniel <danielbalera021@gmail.com>
2025-07-03 19:04:33 -0300

    4 imagens atv

---
+++ event2.js
@@ -0,0 +1,12 @@
+function mouse_over(){
+    document.image.src="image/cat2.jpg";
+}
+function mouse_out() {
+    document.image.src="image/cat.jpg";
+}
+function mouse_over2(){
+    document.image.src="image/nier2.jpg";
+}
+function mouse_out2() {
+    document.image.src="image/nier.jpg";
+}
